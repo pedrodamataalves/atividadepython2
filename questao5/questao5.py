f19154a6688bd1707ef6b246df0b32f33df5f4db
Pedro da Mata Alves <55161573+pedrodamataalves@users.noreply.github.com>
2021-12-08 16:51:01 -0300

    percentual por usuario

--- questao5/questao5.py
+++ questao5/questao5.py
@@ -3,3 +3,7 @@ import pandas as pd
 def conversaodeMB(tamanhoMb):
   tamanhoMb = float(tamanhoMb)
   return (float(tamanhoMb/(1024*1024)))
+
+def percentualPorUsuario(lista, total):
+  percentual = (lista[3]/total)*100
+  lista.insert((len(usuario)),percentual)
